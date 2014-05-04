1b2e8fa5b4e2313cf8ea1febdb09fea8cf85d7c6
Aayush Dawra <aayushgopaldawra@gmail.com>
2014-05-04 10:47:52 -0700

    Added Recipe model

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20140504174737) do
+
+  create_table "recipes", :force => true do |t|
+    t.string   "author"
+    t.string   "steps"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
