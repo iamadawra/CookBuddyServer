06cb1bde33ed7d27f1cbcc69df4b5559c6032429
Aayush Dawra <aayushgopaldawra@gmail.com>
2014-05-04 11:11:06 -0700

    Added annotation

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: recipes
+#
+#  id         :integer          not null, primary key
+#  author     :string(255)
+#  steps      :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  recipename :string(255)
+#
+
 class Recipe < ActiveRecord::Base
   attr_accessible :author, :steps
 end
