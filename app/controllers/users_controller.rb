f325b44a73e8888f2befc4efca516e9c47834563
Aayush Dawra <aayushgopaldawra@gmail.com>
2014-05-04 11:39:50 -0700

    Final fix for User post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
 
-    @user = User.new(:name => params[:name], :author => params[:number], :recipes => params[:recipes])
+    @user = User.new(:name => params[:name], :number => params[:number], :recipes => params[:recipes])
 
     respond_to do |format|
       if @user.save
