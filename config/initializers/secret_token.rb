3e5367510f8f6e96caf2bc88439582326c3e99c9
Aayush Dawra <aayushgopaldawra@gmail.com>
2014-05-04 10:39:29 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CookBuddy::Application.config.secret_token = '5e2525d47e3f5cbb4dbd241afcfc76d701273482855999918068212579c90955a1d91995ec7dbd7e1bfb2584b7411e0d6d10715d0b6ace9cf99f73af0f2bfe8b'
