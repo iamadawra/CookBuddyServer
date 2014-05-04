00513f34ffcfffabb6afa4344622f069f4aa4459
Aayush Dawra <aayushgopaldawra@gmail.com>
2014-05-04 11:16:29 -0700

    Added name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140504180404) do
+ActiveRecord::Schema.define(:version => 20140504181612) do
 
   create_table "recipes", :force => true do |t|
     t.string   "author"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20140504180404) do
     t.string   "recipes"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "name"
   end
 
 end
